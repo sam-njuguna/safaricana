f7f9facf0b584d0869b50360627dd1377383eb63
sam-njuguna <sam.x.njuguna@gmail.com>
2023-05-25 13:24:34 +0300

    (done): completed the whole UI/UX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
 }
